136e3bfead6ab72d911f9ee2580bcce1c6935656
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-03-28 06:49:21 +0100

    source initial commit

---
+++ source/HolisticWare.Core.Graphics.SVG/SVG.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace Core.Graphics.SVG
+{
+    public class SVG
+    {
+        public SVG()
+        {
+            return;
+        }
+
+        public Type[] Types
+        {
+            get;
+            set;
+        }
+
+        public string ToXml()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(svgType), Types);
+
+            string svg = null;
+            using(StringWriter tw = new StringWriter())
+            {
+                serializer.Serialize(tw, this.Content);
+                svg = tw.ToString();
+            }
+
+            return svg;
+        }
+
+        public svgType Content = null;
+
+    }
+
+}
+
